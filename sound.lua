e4bb2a42d0329b5089285f8595818fd6c125ba82
Emeraldsniper <EmeraldsniperXZ@gmail.com>
2024-08-03 22:29:22 -0500

    Fixed sounds thanks to MaiskX3

--- sound.lua
+++ sound.lua
@@ -1,34 +1,34 @@
-smlua_audio_utils_replace_sequence(0x01, 0x22, 75, "01_Seq_custom")
-smlua_audio_utils_replace_sequence(0x02, 0x11, 75, "02_Seq_custom")
-smlua_audio_utils_replace_sequence(0x09, 0x25, 75, "09_Seq_custom")
-smlua_audio_utils_replace_sequence(0x0B, 0x25, 75, "0B_Seq_custom")
-smlua_audio_utils_replace_sequence(0x0C, 0x15, 75, "0C_Seq_custom")
-smlua_audio_utils_replace_sequence(0x0D, 0x25, 75, "0D_Seq_custom")
-smlua_audio_utils_replace_sequence(0x0E, 0x25, 75, "0E_Seq_custom")
-smlua_audio_utils_replace_sequence(0x0F, 0x19, 75, "0F_Seq_custom")
-smlua_audio_utils_replace_sequence(0x10, 0x1B, 75, "10_Seq_custom")
-smlua_audio_utils_replace_sequence(0x12, 0x1F, 75, "12_Seq_custom")
-smlua_audio_utils_replace_sequence(0x14, 0x1A, 75, "14_Seq_custom")
-smlua_audio_utils_replace_sequence(0x15, 0x0E, 75, "15_Seq_custom")
-smlua_audio_utils_replace_sequence(0x16, 0x1A, 75, "16_Seq_custom")
-smlua_audio_utils_replace_sequence(0x17, 0x1A, 75, "17_Seq_custom")
-smlua_audio_utils_replace_sequence(0x1B, 0x0C, 75, "1B_Seq_custom")
-smlua_audio_utils_replace_sequence(0x1C, 0x25, 75, "1C_Seq_custom")
-smlua_audio_utils_replace_sequence(0x1D, 0x0C, 75, "1D_Seq_custom")
-smlua_audio_utils_replace_sequence(0x1E, 0x25, 75, "1E_Seq_custom")
+smlua_audio_utils_replace_sequence(0x01, 0x22, 75, "01_Seq_custom") --star collect
+smlua_audio_utils_replace_sequence(0x02, 0x11, 75, "02_Seq_custom") --title
+smlua_audio_utils_replace_sequence(0x09, 0x25, 75, "09_Seq_custom") --above the clouds
+smlua_audio_utils_replace_sequence(0x0B, 0x25, 75, "0B_Seq_custom") --dangerous minigame
+smlua_audio_utils_replace_sequence(0x0C, 0x15, 75, "0C_Seq_custom") --water temple
+smlua_audio_utils_replace_sequence(0x0D, 0x25, 75, "0D_Seq_custom") --star select
+smlua_audio_utils_replace_sequence(0x0E, 0x25, 75, "0E_Seq_custom") --happy minigame
+smlua_audio_utils_replace_sequence(0x0F, 0x19, 75, "0F_Seq_custom") --protect the cake
+smlua_audio_utils_replace_sequence(0x10, 0x1B, 75, "10_Seq_custom") --the villain appears
+smlua_audio_utils_replace_sequence(0x12, 0x1F, 75, "12_Seq_custom") --highscore
+smlua_audio_utils_replace_sequence(0x14, 0x1A, 75, "14_Seq_custom") --race fanfare
+smlua_audio_utils_replace_sequence(0x15, 0x0E, 75, "15_Seq_custom") --star spawn
+smlua_audio_utils_replace_sequence(0x16, 0x1A, 75, "16_Seq_custom") --boss theme B
+smlua_audio_utils_replace_sequence(0x17, 0x1A, 75, "17_Seq_custom") --collect key
+smlua_audio_utils_replace_sequence(0x1B, 0x0C, 75, "1B_Seq_custom") --solve puzzle
+smlua_audio_utils_replace_sequence(0x1C, 0x25, 75, "1C_Seq_custom") --toad message
+smlua_audio_utils_replace_sequence(0x1D, 0x0C, 75, "1D_Seq_custom") --peach message
+smlua_audio_utils_replace_sequence(0x1E, 0x25, 75, "1E_Seq_custom") --
 smlua_audio_utils_replace_sequence(0x1F, 0x1A, 75, "1F_Seq_custom")
 smlua_audio_utils_replace_sequence(0x20, 0x1B, 75, "20_Seq_custom")
 smlua_audio_utils_replace_sequence(0x21, 0x24, 75, "21_Seq_custom")
-smlua_audio_utils_replace_sequence(0x23, 0x25, 75, "23_Seq_custom")
+smlua_audio_utils_replace_sequence(0x23, 0x25, 75, "23_Seq_custom") --wildlife valley
 smlua_audio_utils_replace_sequence(0x24, 0x25, 75, "24_Seq_custom")
 smlua_audio_utils_replace_sequence(0x27, 0x25, 75, "27_Seq_custom")
 smlua_audio_utils_replace_sequence(0x28, 0x25, 75, "28_Seq_custom")
 smlua_audio_utils_replace_sequence(0x29, 0x25, 75, "29_Seq_custom")
-smlua_audio_utils_replace_sequence(0x2D, 0x25, 75, "2D_Seq_custom")
+smlua_audio_utils_replace_sequence(0x2D, 0x25, 75, "2D_Seq_custom") --cave theme
 smlua_audio_utils_replace_sequence(0x35, 0x10, 75, "35_Seq_custom")
 smlua_audio_utils_replace_sequence(0x36, 0x22, 75, "36_Seq_custom")
 smlua_audio_utils_replace_sequence(0x3C, 20, 75, "3C_Seq_custom")
 smlua_audio_utils_replace_sequence(0x3E, 0x0C, 75, "3E_Seq_custom")
 smlua_audio_utils_replace_sequence(0x40, 0x11, 75, "40_Seq_custom")
 smlua_audio_utils_replace_sequence(0x41, 0x25, 75, "41_Seq_custom")
-smlua_audio_utils_replace_sequence(0x43, 0x0D, 75, "43_Seq_custom")
+smlua_audio_utils_replace_sequence(0x43, 0x2A, 75, "43_Seq_custom") --rainbow road
