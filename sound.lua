934a41b866363be932b7280fcabb84dc19952950
MaiskX3 <maiskxdxd@gmail.com>
2024-09-08 15:56:26 -0300

    Add files via upload

--- sound.lua
+++ sound.lua
@@ -1,6 +1,6 @@
 smlua_audio_utils_replace_sequence(0x01, 0x22, 75, "01_Seq_custom") --star collect
 --smlua_audio_utils_replace_sequence(0x02, 0x11, 75, "02_Seq_custom") --title
-smlua_audio_utils_replace_sequence(0x06, 0x25, 75, "06_Seq_custom")
+smlua_audio_utils_replace_sequence(0x06, 0x25, 75, "06_Seq_custom") --snake temple
 smlua_audio_utils_replace_sequence(0x09, 0x25, 75, "09_Seq_custom") --above the clouds
 smlua_audio_utils_replace_sequence(0x0B, 0x25, 75, "0B_Seq_custom") --dangerous minigame
 smlua_audio_utils_replace_sequence(0x0C, 0x15, 75, "0C_Seq_custom") --water temple
@@ -8,42 +8,42 @@ smlua_audio_utils_replace_sequence(0x0D, 0x25, 75, "0D_Seq_custom") --star selec
 smlua_audio_utils_replace_sequence(0x0E, 0x25, 75, "0E_Seq_custom") --happy minigame
 smlua_audio_utils_replace_sequence(0x0F, 0x19, 75, "0F_Seq_custom") --protect the cake
 smlua_audio_utils_replace_sequence(0x10, 0x1B, 75, "10_Seq_custom") --the villain appears
-smlua_audio_utils_replace_sequence(0x11, 0x19, 75, "11_Seq_custom")
+smlua_audio_utils_replace_sequence(0x11, 0x19, 75, "11_Seq_custom") --bowser's fiery castle
 smlua_audio_utils_replace_sequence(0x12, 0x1F, 75, "12_Seq_custom") --highscore
 smlua_audio_utils_replace_sequence(0x14, 0x1A, 75, "14_Seq_custom") --race fanfare
 smlua_audio_utils_replace_sequence(0x15, 0x0E, 75, "15_Seq_custom") --star spawn
 smlua_audio_utils_replace_sequence(0x16, 0x1A, 75, "16_Seq_custom") --boss theme B
 smlua_audio_utils_replace_sequence(0x17, 0x1A, 75, "17_Seq_custom") --collect key
-smlua_audio_utils_replace_sequence(0x1A, 0x1B, 75, "1A_Seq_custom")
+smlua_audio_utils_replace_sequence(0x1A, 0x1B, 75, "1A_Seq_custom") --rashay's moon cellar
 smlua_audio_utils_replace_sequence(0x1B, 0x0C, 75, "1B_Seq_custom") --solve puzzle
-smlua_audio_utils_replace_sequence(0x46, 0x25, 75, "1C_Seq_custom") ----final boss
-smlua_audio_utils_replace_sequence(0x1D, 0x0C, 75, "1D_Seq_custom") --peach message
-smlua_audio_utils_replace_sequence(0x1E, 0x25, 75, "1E_Seq_custom") --
-smlua_audio_utils_replace_sequence(0x1F, 0x1A, 75, "1F_Seq_custom")
-smlua_audio_utils_replace_sequence(0x20, 0x1B, 75, "20_Seq_custom")
-smlua_audio_utils_replace_sequence(0x21, 0x24, 75, "21_Seq_custom")
+smlua_audio_utils_replace_sequence(0x46, 0x25, 75, "1C_Seq_custom") --rashay phase 1
+smlua_audio_utils_replace_sequence(0x1D, 0x0C, 75, "1D_Seq_custom") --intro cutscene
+smlua_audio_utils_replace_sequence(0x1E, 0x25, 75, "1E_Seq_custom") --bob-omb village
+smlua_audio_utils_replace_sequence(0x1F, 0x1A, 75, "1F_Seq_custom") --rashay defeat
+smlua_audio_utils_replace_sequence(0x20, 0x1B, 75, "20_Seq_custom") --
+smlua_audio_utils_replace_sequence(0x21, 0x24, 75, "21_Seq_custom") --rashay phase 2
 smlua_audio_utils_replace_sequence(0x23, 0x25, 75, "23_Seq_custom") --wildlife valley
-smlua_audio_utils_replace_sequence(0x24, 0x25, 75, "24_Seq_custom")
-smlua_audio_utils_replace_sequence(0x25, 0x11, 75, "25_Seq_custom")
-smlua_audio_utils_replace_sequence(0x27, 0x25, 75, "27_Seq_custom")
-smlua_audio_utils_replace_sequence(0x28, 0x25, 75, "28_Seq_custom")
-smlua_audio_utils_replace_sequence(0x29, 0x25, 75, "29_Seq_custom")
-smlua_audio_utils_replace_sequence(0x2B, 0x25, 75, "2B_Seq_custom")
+smlua_audio_utils_replace_sequence(0x24, 0x25, 75, "24_Seq_custom") --stonesnake shatters
+smlua_audio_utils_replace_sequence(0x25, 0x11, 75, "25_Seq_custom") --sunset islet
+smlua_audio_utils_replace_sequence(0x27, 0x25, 75, "27_Seq_custom") --abandoned outpost
+smlua_audio_utils_replace_sequence(0x28, 0x25, 75, "28_Seq_custom") --super sweet sugar land
+smlua_audio_utils_replace_sequence(0x29, 0x25, 75, "29_Seq_custom") --shadow factory
+smlua_audio_utils_replace_sequence(0x2B, 0x25, 75, "2B_Seq_custom") --skyhigh islands
 smlua_audio_utils_replace_sequence(0x2D, 0x25, 75, "2D_Seq_custom") --cave theme
-smlua_audio_utils_replace_sequence(0x2E, 0x25, 75, "2E_Seq_custom")
-smlua_audio_utils_replace_sequence(0x2F, 0x19, 75, "2F_Seq_custom")
-smlua_audio_utils_replace_sequence(0x31, 0x15, 75, "31_Seq_custom")
-smlua_audio_utils_replace_sequence(0x32, 0x25, 75, "32_Seq_custom")
-smlua_audio_utils_replace_sequence(0x34, 0x25, 75, "34_Seq_custom")
+smlua_audio_utils_replace_sequence(0x2E, 0x25, 75, "2E_Seq_custom") --slider theme
+smlua_audio_utils_replace_sequence(0x2F, 0x19, 75, "2F_Seq_custom") --melty molten mountains fire side
+smlua_audio_utils_replace_sequence(0x31, 0x15, 75, "31_Seq_custom") --dusky doomed dale
+smlua_audio_utils_replace_sequence(0x32, 0x25, 75, "32_Seq_custom") --boo dance
+smlua_audio_utils_replace_sequence(0x34, 0x25, 75, "34_Seq_custom") --file select
 smlua_audio_utils_replace_sequence(0x35, 0x2A, 75, "35_Seq_custom") --drybone desert
 smlua_audio_utils_replace_sequence(0x36, 0x22, 75, "36_Seq_custom") --harried honey hive
-smlua_audio_utils_replace_sequence(0x38, 0x17, 75, "38_Seq_custom")
-smlua_audio_utils_replace_sequence(0x3A, 0xC, 80, "3A_Seq_custom")
+smlua_audio_utils_replace_sequence(0x38, 0x17, 75, "38_Seq_custom") --giant desk
+smlua_audio_utils_replace_sequence(0x3A, 0x2A, 80, "3A_Seq_custom") --big beast's belly
 smlua_audio_utils_replace_sequence(0x3C, 0x14, 80, "3C_Seq_custom") --broken song //uses streamed music
-smlua_audio_utils_replace_sequence(0x3D, 25, 80, "3D_Seq_custom") -- virus boss
-smlua_audio_utils_replace_sequence(0x3F, 0x25, 75, "3F_Seq_custom")
+smlua_audio_utils_replace_sequence(0x3D, 0x25, 80, "3D_Seq_custom") -- virus boss
+smlua_audio_utils_replace_sequence(0x3F, 0x25, 75, "3F_Seq_custom") --secret aquarium
 smlua_audio_utils_replace_sequence(0x3E, 0x0C, 75, "3E_Seq_custom") --starlight zone
 smlua_audio_utils_replace_sequence(0x40, 0x11, 75, "40_Seq_custom") --gamble game gallery
 smlua_audio_utils_replace_sequence(0x41, 0x25, 75, "41_Seq_custom") --underground sewer
-smlua_audio_utils_replace_sequence(0x42, 0x18, 75, "42_Seq_custom")
+smlua_audio_utils_replace_sequence(0x42, 0x18, 75, "42_Seq_custom") --livid surreal dream
 smlua_audio_utils_replace_sequence(0x43, 0x2A, 75, "43_Seq_custom") --rainbow road
